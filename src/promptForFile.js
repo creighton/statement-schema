50cbdd7517f855e7ab6a718536e12f94cb1751ed
Andy Creighton <cr8onski@gmail.com>
2017-05-18 10:24:17 -0400

    add command line prompt for filemname basic functionality, rearrange test statements into folders

--- src/promptForFile.js
+++ src/promptForFile.js
@@ -6,7 +6,25 @@
     module.exports = function (cb) {
         console.log('promptForFile');
         const parseFiles = require('./parseFiles.js');
-        parseFiles(['various.json'], cb);
+
+
+        const readline = require('readline');
+
+        const rl = readline.createInterface({
+          input: process.stdin,
+          output: process.stdout
+        });
+
+        rl.question('Enter path and filename: ', (answer) => {
+          // TODO: Log the answer in a database
+          console.log(`Thank you for your valuable feedback: ${typeof answer}`);
+console.log(answer);
+          rl.close();
+          parseFiles([answer] || ['various.json'], cb);
+        });
+
+
+
         // cb(null, ['statements/various.json']);
     }
 );  // end closure
