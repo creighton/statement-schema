ac11e6b7e86101a98e9d275c5b2b66667b1bd086
Andy Creighton <cr8onski@gmail.com>
2017-08-21 15:00:09 -0400

    it took a solar eclipse, but added code and group example to authority

--- src/validateAuthority.js
+++ src/validateAuthority.js
@@ -13,24 +13,34 @@ module.exports = function (authority, cb) {
 
     let str = __dirname;
     let msg = 'WARNING: Authority is to be set by the LRS, not LRP.  If an LRS receives a statement with the authority property set, the LRS should overwrite it.';
+    str = str.replace('src', 'test/schemas/');
 
     if (authority.objectType && authority.objectType === "Group") {
-        str = str.replace('src', 'test/schemas/idgroup');
-        let valid = v.validate(require(str), authority);
+        let valid = v.validate(require(str + 'anongroup'), authority);
 
         if (!valid) {
             msg += '\n\tauthority errors - ' + v.errorsText();
         } else {
-            msg += '\n\tauthority - validated';
+            msg += '\n\tauthority Anonymous Group - validated';
+        }
+        // items in a member array must validate as agents
+        if (authority.member) {
+            for (let act of authority.member) {
+                if (v.validate(require(str + 'agent'), act)) {
+                    msg += `\n\t\tgroup member ${act.name} - is a valid agent`;
+                }
+                else {
+                    msg += `\n\t\tgroup member ${JSON.stringify(act)} - is not a valid agent\n\t\t${v.errorsText()}`;
+                }
+            }
         }
     } else {    // the authority must be an agent
-        str = str.replace('src', 'test/schemas/agent');
-        let valid = v.validate(require(str), authority);
+        let valid = v.validate(require(str + 'agent'), authority);
 
         if (!valid) {
             msg += '\n\tauthority errors - ' + v.errorsText();
         } else {
-            msg += '\n\tauthority - validated';
+            msg += '\n\tauthority Agent - validated';
         }
     }
     cb(null, msg);
